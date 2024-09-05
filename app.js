04f0f15a3653c2430dec578549676479f0fdab5a
bocon3 <boconmarkangelo7782gmail.com>
2024-09-05 12:58:54 +0800

    Hello

---
+++ app.js
@@ -0,0 +1,63 @@
+// Data types
+//String
+
+let name = "mark";
+console.log("Your name: " + name);
+let age = 20;
+console.log ("Your age: " + age);
+
+ 
+
+//Arithmetic and Logical Operators
+
+let a = 100;
+let b = 20;
+let diff = a - b;
+console.log("My grades: " + diff);
+
+
+//Conditional Statement
+
+if(age>=50){
+    console.log("gawas Ka parts");
+}else;
+    console.log(" sulod ka parts");
+
+
+    let grado = 80;
+    let category;
+
+    switch(true){
+        case (grado>=75):
+            category = "Pasar ka";
+            break;
+            default:
+                category = "Bagsak ka";
+                break;
+
+                console.log("Naka survive pa");
+
+                //loops
+
+                const activities = ["dula","kaon","laag"];
+                for (let w = 0; w < activities.length; w++){
+                    console.assert;ongamepadconnected('activities ${w + 1}: ${activities[w]}');
+
+                }
+}
+// Functional Programming
+// Determine odd or even
+
+function CheckNumber(Number){
+    const numberToCheck = 30;
+    if (number % 2 === 0){
+        console.log('${number} is even.');
+    }else if (isPrime(number)) {
+        console.log('${number} is prime.');
+    }else{
+        console.log('${number} is niether even nor prime.');
+    }
+    }
+    console.log("the number is prime");
+    
+    
