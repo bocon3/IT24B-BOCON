3f18002ca0039c83598a8f3710ac8ee115514344
bocon3 <boconmarkangelo778@gmail.com>
2024-10-08 15:02:05 +0800

    modefied map locations

--- applet-1.js
+++ applet-1.js
@@ -30,8 +30,8 @@ const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
 
     
 myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
-myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
-myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+myMap.addMarker(8.359131,124.868615, 'BA Building');
+myMap.addMarker(8.360256,124.867524, 'SC Building');S
 
 myMap.loadMarkersFromJson('applet-2.json');
     
